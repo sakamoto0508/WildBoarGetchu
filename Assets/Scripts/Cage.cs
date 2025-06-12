a0d772803c24f4e2db216c19126de2f3554e36ed
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-06-12 16:02:01 +0900

    [add]cage

---
+++ Assets/Scripts/Cage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Cage : MonoBehaviour
+{
+    //EnemyBase _enemyContainer;
+    private void Start()
+    {
+        // _enemyContainer = FindAnyObjectByType<EnemyBase>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<EnemyBase>(out EnemyBase enemyBase) &&
+            collision.gameObject.tag == "Enemy" && enemyBase.enemyState == EnemyBase.EnemyState.ChaseCage)
+        {
+            enemyBase.enemyState = EnemyBase.EnemyState.Wander;
+            Invoke(nameof(ActiveCage), 5f);
+            this.gameObject.SetActive(false);
+        }
+    }
+    private void ActiveCage()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/Player/Getchu.cs
+++ Assets/Scripts/Player/Getchu.cs
@@ -4,21 +4,26 @@ using UnityEngine.AI;
 public class Getchu : MonoBehaviour
 {
     PlayerContainer _playerContainer;
-    EnemyBase _enemyContainer;
+    //EnemyBase _enemyContainer;
     [SerializeField] private GameObject _teleportPoint;
     private void Start()
     {
         _playerContainer = FindAnyObjectByType<PlayerContainer>();
-        _enemyContainer = FindAnyObjectByType<EnemyBase>();
+        //_enemyContainer = FindAnyObjectByType<EnemyBase>();
     }
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.tag == "Enemy" && _playerContainer.state == PlayerContainer.MyState.Attack)
         {
-            Debug.Log("“G‚É“–‚½‚Á‚½");
+            Debug.Log($"“G‚É“–‚½‚Á‚½ {other.name}");
             other.transform.position = _teleportPoint.transform.position;
-            _enemyContainer.enemyState = EnemyBase.EnemyState.Getchued;
+
+            if(other.gameObject.TryGetComponent<EnemyBase>(out EnemyBase enemyBase))
+            {
+                enemyBase.enemyState = EnemyBase.EnemyState.Getchued;
+            }
+            
             other.gameObject.GetComponent<NavMeshAgent>().enabled = false;
         }
     }
